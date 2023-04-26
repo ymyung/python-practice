1b145bf720638326f5829cfdfc0672c91ab2ab85
Yo Myung <yosepmyung@gmail.com>
2023-04-26 14:43:59 -0600

    python practice

--- practice.py
+++ practice.py
@@ -4,5 +4,38 @@ num = True
 
 print(num)
 print('hello world')
-name = input('input: ')
-print('hello ' + name)
+# name = input('input: ')
+# print('hello ' + name)
+
+int()
+float()
+bool()
+str()
+
+print(11/3)
+print(11//3)
+print(11 % 3)
+
+x = 5
+x += 5
+print(x)
+
+y = 5 > 3
+z = 5 <= 3
+aa = 5 != 4
+print(y)
+print(z)
+print(aa)
+
+price = 10
+print(price > 100 or price == 10)
+
+if 5 > 4:
+    print('5 is greater then 4')
+else:
+    print('5 is less then 4')
+
+# comment
+
+weight = input('Weight: ')
+print('you weight ' + weight)
